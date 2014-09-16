d43b32b2fae1fbf00d4c2dcf485ecaade80ac48b
multimike77 <multimike77@gmail.com>
2014-09-16 22:23:20 +0200

    exercise 4.1, 4.2

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -52,9 +52,16 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs).flatMap(m =>
+      mean(xs.map(x => math.pow(x - m, 2)))
+    )
+  }
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a.flatMap(av => b.map(bv => f(av, bv)))
+  }
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
 
