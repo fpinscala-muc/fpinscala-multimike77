dbf6dfc225ddccaa5f634aafb3d26d5999a1f3a3
multimike <multimike77@gmail.com>
2014-11-11 20:27:25 +0100

    6.9

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -111,9 +111,19 @@ object RNG {
   def nonNegativeLessThan(n: Int): Rand[Int] = ???
 
   // 6.9
-  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] = ???
+  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] = {
+    flatMap(s) {a =>
+      unit(f(a))
+    }
+  }
 
-  def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra) { a =>
+      map(rb) { b =>
+        f(a, b)
+      }
+    }
+  }
 }
 
 case class State[S,+A](run: S => (A, S)) {
