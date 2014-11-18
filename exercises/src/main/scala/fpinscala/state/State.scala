2b9f43679b8ed362dbaeaa6b10edbb1af437c93d
multimike <multimike77@gmail.com>
2014-11-18 21:28:21 +0100

    6.10 map, flatMap, map2, unit

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -127,12 +127,26 @@ object RNG {
 }
 
 case class State[S,+A](run: S => (A, S)) {
+
+  // 6.10
   def map[B](f: A => B): State[S, B] =
-    sys.error("todo")
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    sys.error("todo")
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    sys.error("todo")
+    flatMap { a =>
+      State.unit(f(a))
+    }
+
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = {
+    flatMap { a =>
+      sb.map {b =>
+        f(a, b)
+      }
+    }
+  }
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+
 }
 
 sealed trait Input
@@ -144,7 +158,9 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 object State {
   type Rand[A] = State[RNG, A]
 
-  def unit[S, A](a: A): State[S, A] = ???
+  def unit[S, A](a: A): State[S, A] = {
+    State(s => (a, s))
+  }
 
   def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] = ???
 
